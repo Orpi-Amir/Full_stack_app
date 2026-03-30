58c20859c2ae806545594d31e12ea5a791c1ed81
Orpi-Amir <6220435bh@std.aou.org.bh>
2026-03-30 14:28:48 +0300

    Fix MongoDB connection bug in utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 let connection = {};
 
 export const connectToDb = async () => {
-  if (connection.isconnected) return;
+  if (connection.isConnected) return;
 
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
-    connection.isconnected = db.connections[0].readyState;
-    console.log("✅ Connected to MongoDB:", db.connection.name);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("✅ Connected to MongoDB:", db.connections[0].name);
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
     throw error;
